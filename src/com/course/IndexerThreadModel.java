a4dd0151b417e6e062f9a61a85c356adee5d27ce
Toma <aromatmail@gmail.com>
2022-01-23 23:27:33 +0200

    (Model). Added an indexer model

---
+++ src/com/course/IndexerThreadModel.java
@@ -0,0 +1,48 @@
+package com.course;
+
+import java.io.FileNotFoundException;
+import java.nio.file.Path;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import com.course.Helper.*;
+
+public class IndexerThreadModel extends Thread {
+    ConcurrentHashMap<String, Set<Path>> invertedIndexMap;
+    List<Path> filePaths;
+    Integer threadNumber, threadsQuantity;
+
+    public IndexerThreadModel(ConcurrentHashMap<String, Set<Path>> invertedIndexMap, List<Path> filePaths,
+                              Integer threadNumber, Integer maxThreads) {
+        this.invertedIndexMap = invertedIndexMap;
+        this.filePaths = filePaths;
+        this.threadNumber = threadNumber;
+        this.threadsQuantity = maxThreads;
+    }
+
+    @Override
+    public void run() {
+        List<Path> files = FileHelper.getPathList(filePaths, threadNumber,threadsQuantity);
+        for (Path path : files) {
+            Scanner scanner = null;
+            try {
+                scanner = new Scanner(path.toFile());
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            while (Objects.requireNonNull(scanner).hasNext()) {
+
+                String word = FileHelper.normalizeWord(scanner.next());
+
+                if (!invertedIndexMap.containsKey(word)) {
+                    Set<Path> values = new HashSet<>();
+                    values.add(path);
+                    invertedIndexMap.putIfAbsent(word, values);
+                } else {
+                    Set<Path> values = invertedIndexMap.get(word);
+                    values.add(path);
+                    invertedIndexMap.replace(word, values);
+                }
+            }
+        }
+    }
+}
