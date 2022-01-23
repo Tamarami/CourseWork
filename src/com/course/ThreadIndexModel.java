d1fd9ea963f1f0fb18a10e8cf25fcee9e956d6a1
Toma <aromatmail@gmail.com>
2022-01-23 21:48:45 +0200

    Added base files

---
+++ src/com/course/ThreadIndexModel.java
@@ -0,0 +1,12 @@
+package com.course;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ThreadIndexModel {
+    public static final String BASE_PATH = "D:\\JavaLabs\\CourseWParallel\\aclImdb\\";
+    public List<String> pathParts = new ArrayList<String>();
+
+
+
+}
